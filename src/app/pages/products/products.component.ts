139306377668e391496282aa1777d1d04cb2f61e
daibede <xuandai300092@gmail.com>
2022-05-12 08:49:37 +0700

    update List Products

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
+  isStatus: boolean = false;
+  nameInput: string = "";
+  productList: {id: number, name: string, price: number , status: boolean}[]= [
+    {id:1, name: 'Products A', price: 500, status: true},
+    {id:2, name: 'Products B', price: 600, status: false},
+    {id:3, name: 'Products C', price: 800, status: true},
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onhandleClick(){
+    this.isStatus = !this.isStatus
+  }
+  onHandeleRemove(id: number){
+    this.productList = this.productList.filter(products => products.id !== id )
+  }
 }
