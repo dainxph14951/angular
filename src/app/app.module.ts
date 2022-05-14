b0b056bc2028bd5b99879713695349d328021c43
daibede <xuandai300092@gmail.com>
2022-05-14 07:56:51 +0700

    products detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { ProductsDetailComponent } from './pages/products-detail/products-detail/products-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    ProductsDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Iproducts } from 'src/app/models/products.models';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  productsDetail! : Iproducts
   isStatus: boolean = false;
   nameInput: string = "";
-  productList: {id: number, name: string, price: number , status: boolean}[]= [
+  productList: Iproducts[]= [
     {id:1, name: 'Products A', price: 500, status: true},
     {id:2, name: 'Products B', price: 600, status: false},
     {id:3, name: 'Products C', price: 800, status: true},
@@ -22,5 +23,9 @@ export class ProductsComponent implements OnInit {
   }
   onHandeleRemove(id: number){
     this.productList = this.productList.filter(products => products.id !== id )
+  }
+  onHandeleEdit(products: Iproducts) {
+    console.log(1);
+    this.productsDetail = products;
   }
 }
